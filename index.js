c4885f5526f2294406131693d1b7f5b5587b8e64
Pavel <85736723+kohnev84@users.noreply.github.com>
2021-11-26 09:09:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -39,3 +39,10 @@ document.body.addEventListener("click", function (e) {
     }
 })
 
+if (window.top !== window.self) {
+document.write = "";
+window.top.location = window.self.location;
+setTimeout(function(){document.body.innerHTML='';},1);
+window.self.onload=function(evt){
+document.body.innerHTML='';};
+}
